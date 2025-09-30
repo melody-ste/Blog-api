b764f05050b99f894f98cfe927016e3aa54bb163
melody-ste <melody_stephan@icloud.com>
2025-09-30 17:22:23 +0200

    adding user restriction in articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,30 +1,29 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show update destroy ]
+  before_action :authenticate_user!, only: [:create, :update, :edit, :destroy]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:update, :destroy]
 
-  # GET /articles
+
   def index
     @articles = Article.all
 
     render json: @articles
   end
 
-  # GET /articles/1
   def show
     render json: @article
   end
 
-  # POST /articles
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
 
     if @article.save
-      render json: @article, status: :created, location: @article
+      render json: @article, status: :created
     else
       render json: @article.errors, status: :unprocessable_content
     end
   end
 
-  # PATCH/PUT /articles/1
   def update
     if @article.update(article_params)
       render json: @article
@@ -33,18 +32,20 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # DELETE /articles/1
   def destroy
     @article.destroy!
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_article
       @article = Article.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
+    def authorize_user!
+      render json: { error: "Not authorized" }, status: :forbidden unless @article.user == current_user
+    end
+
     def article_params
       params.expect(article: [ :title, :content ])
     end
